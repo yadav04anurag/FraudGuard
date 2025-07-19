2713a02c154c7254fe14c135be0ae7763a931007
yadav04anurag <164538440+yadav04anurag@users.noreply.github.com>
2025-07-19 23:43:18 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://fraud-guard-bnc9.vercel.app/api',
+  baseURL: 'https://fraud-guard-pi.vercel.app/api',
   withCredentials: true,
 });
 

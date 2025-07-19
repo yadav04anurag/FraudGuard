af88e97a00fefe349ec597c07ef874a7a862b8d0
yadav04anurag <yadavanurag1403@gmail.com>
2025-07-19 21:23:31 +0530

    deploy changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
+   build: { manifest: true, outDir: './dist'},
+  base:  '/',
+  root: './src',
   plugins: [react(),tailwindcss()],
 })
